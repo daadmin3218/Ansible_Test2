9a9d2898aba392e1a1ee8024cfb04193d444e7bf
Jam <j.may.xxx@gmail.com>
2025-05-18 13:05:01 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,8 +2,8 @@ def factorial(n):
     if n == 0:
         return 1
     else:
-        return: n * facorial(n-1)
+        return n * factorial(n-1)
 
-number = 5
-results = facorial(number)
-print(f"The facorial of {number} is {result}."}
+number = 4
+results = factorial(number)
+print(f"The facorial of {number} is {results}.")
